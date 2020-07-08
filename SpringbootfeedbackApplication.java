08abc2aec70234f9f907a12af29f651e9211ebd6
Narraendharmass <67266747+Narraendharmass@users.noreply.github.com>
2020-07-08 22:53:03 +0530

    Add files via upload

---
+++ SpringbootfeedbackApplication.java
@@ -0,0 +1,13 @@
+package com.feedback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootfeedbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootfeedbackApplication.class, args);
+	}
+
+}
